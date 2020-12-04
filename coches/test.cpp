4de617b06688b5f56634884e8173efab1e7f8627
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2020-12-03 18:36:02 -0600

    Added test

--- coches/test.cpp
+++ coches/test.cpp
@@ -9,6 +9,8 @@ int main()
     AutoElectrico eCoche;
     AutoNormal gasCoche;
 
-    AutoElectrico teslaModelY('Tesla', 'ModelY', 2020, 300, 4, 300);
-    AutoNormal vwBocho('Volkswagen', 'Bocho', 1980, 1000, 4, 75);
+    AutoElectrico teslaModelY("Tesla", "ModelY", 2020, 300, 4, 300);
+    AutoNormal vwBocho("Volkswagen", "Bocho", 1980, 1000, 4, 75);
+
+    cout << vwBocho.getNombre() << "y" << teslaModelY.getNombre();
 };
